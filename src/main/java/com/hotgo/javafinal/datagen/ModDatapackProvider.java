15de8f0bc222d875a569f0566539bd5bf985ed18
Jonah <hotgo343@gmail.com>
2026-05-06 00:18:35 -0400

    Started basis for world generation

--- src/main/java/com/hotgo/javafinal/datagen/ModDatapackProvider.java
+++ src/main/java/com/hotgo/javafinal/datagen/ModDatapackProvider.java
@@ -4,11 +4,15 @@ import com.hotgo.javafinal.JavaFinal;
 import com.hotgo.javafinal.enchantment.ModEnchantments;
 import com.hotgo.javafinal.trim.ModTrimMaterials;
 import com.hotgo.javafinal.trim.ModTrimPatterns;
+import com.hotgo.javafinal.worldgen.ModBiomeModifiers;
+import com.hotgo.javafinal.worldgen.ModConfiguredFeatures;
+import com.hotgo.javafinal.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -17,7 +21,11 @@ public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
             .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
-            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(JavaFinal.MOD_ID));

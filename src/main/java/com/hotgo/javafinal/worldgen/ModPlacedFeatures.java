dffb90903645493b325ca1bb9b7ad50cb64385c4
Jonah <hotgo343@gmail.com>
2026-05-07 00:35:24 -0400

    Added bloodwood tree world gen

--- src/main/java/com/hotgo/javafinal/worldgen/ModPlacedFeatures.java
+++ src/main/java/com/hotgo/javafinal/worldgen/ModPlacedFeatures.java
@@ -1,9 +1,12 @@
 package com.hotgo.javafinal.worldgen;
 
 import com.hotgo.javafinal.JavaFinal;
+import com.hotgo.javafinal.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -19,6 +22,7 @@ public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> NETHER_RED_MAGIC_ORE_PLACED_KEY = registerKey("nether_red_magic_ore_placed");
     public static final ResourceKey<PlacedFeature> END_RED_MAGIC_ORE_PLACED_KEY = registerKey("end_red_magic_ore_placed");
 
+    public static final ResourceKey<PlacedFeature> BLOODWOOD_PLACED_KEY = registerKey("bloodwood_placed");
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
@@ -39,6 +43,15 @@ public class ModPlacedFeatures {
 
         register(context, END_RED_MAGIC_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_RED_MAGIC_ORE_KEY),
                 ModOrePlacement.commonOrePlacement(12, HeightRangePlacement.uniform(VerticalAnchor.absolute(1), VerticalAnchor.absolute(80))));
+
+        //countExtra placement
+        //baseValue is how many to spawn, however there is a chance*100=percent that the addedAmmount will also spawn
+        //if 1/chance is NOT an integer it will not work
+        //1/0.1 = 10 -> completely fine
+        //1/0.3 = 3.3... -> will not work
+        register(context, BLOODWOOD_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.BLOODWOOD_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2),
+                        ModBlocks.BLOODWOOD_SAPLING.get()));
 
 
     }
